dd93507949a51fbbdc81dff38e00a584f4efdc91
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2016-09-10 19:31:00 +0300

    Command line arguments were added

--- main.go
+++ main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 )
+
+type UriParams struct {
+	Url    string
+	Method string
+}
+
+func parseArguments() (*UriParams, error) {
+	url := flag.String("url", "http://localhost:8080", "requesting url")
+	isGet := flag.Bool("get", false, "get method")
+	isPost := flag.Bool("post", false, "post method")
+	isHead := flag.Bool("head", false, "head method")
+
+	flag.Parse()
+
+	if len(*url) <= 0 {
+		return nil, errors.New("Plese specify url")
+	}
+
+	var method string
+	if *isGet {
+		method = "GET"
+	} else if *isPost {
+		method = "POST"
+	} else if *isHead {
+		method = "HEAD"
+	}
+
+	if len(method) <= 0 {
+		return nil, errors.New("Invalid http method")
+	}
+
+	return &UriParams{
+		Url:    *url,
+		Method: method,
+	}, nil
+}
 
 func printResult(response *http.Response, err error) {
 	if err != nil {
@@ -36,6 +74,25 @@ func Get(url string) {
 	printResult(response, err)
 }
 
+func doRequest(params *UriParams) {
+	request, err := http.NewRequest(params.Method, params.Url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	printResult(response, err)
+}
+
 func main() {
-	Get("http://tut.by")
+	uriParams, err := parseArguments()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	doRequest(uriParams)
 }
