49e1cedb8ec4a7f784326ae4e89b73df8909f521
Michael <michael.mikhalevich@gmail.com>
2025-09-16 23:08:41 +0300

    refactor old code (#1)

---
+++ internal/request/request.go
@@ -0,0 +1,64 @@
+package request
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+)
+
+type Interceptor interface {
+	Before()
+	After(rsp *http.Response)
+	Redirect(rsp *http.Response)
+}
+
+type Request struct {
+	interceptor Interceptor
+}
+
+type RedirectFunc func(request *http.Request, via []*http.Request) error
+
+func New(interceptor Interceptor) *Request {
+	return &Request{
+		interceptor: interceptor,
+	}
+}
+
+func (r *Request) Do(ctx context.Context, method, url string, forceHTTP11 bool) error {
+	request, err := http.NewRequestWithContext(ctx, method, url, nil)
+	if err != nil {
+		return fmt.Errorf("create new request: %w", err)
+	}
+
+	client := &http.Client{
+		CheckRedirect: r.doRedirect(),
+	}
+
+	if forceHTTP11 {
+		client.Transport = &http.Transport{
+			TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{},
+		}
+	}
+
+	r.interceptor.Before()
+
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("do http request: %w", err)
+	}
+
+	defer response.Body.Close()
+
+	r.interceptor.After(response)
+
+	return nil
+}
+
+func (r *Request) doRedirect() RedirectFunc {
+	return func(request *http.Request, via []*http.Request) error {
+		r.interceptor.Redirect(request.Response)
+
+		return nil
+	}
+}
